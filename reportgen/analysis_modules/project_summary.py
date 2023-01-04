ade6b479277838c6d72f3725775114ef8d07460f
Bashir Rastegarpanah <bashir@Bashirs-MacBook-Pro-2.local>
2023-01-03 17:34:30 -0800

    polish

--- reportgen/analysis_modules/project_summary.py
+++ reportgen/analysis_modules/project_summary.py
@@ -2,6 +2,7 @@ from .base import BaseAnalysis
 from ..output_modules import SimpleTextBlock, FormattedTextBlock, SimpleImage, FormattedTextStyle, SimpleTextStyle
 from ..output_modules.text_styles import PlainText, BoldText, ItalicText
 
+
 class ProjectSummary(BaseAnalysis):
     def __init__(self, project_id):
         self.project_id = project_id
