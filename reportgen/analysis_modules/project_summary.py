da2c0ecf25bb1eeb8c864408257e63350ead4691
Bashir Rastegarpanah <bashir@Bashirs-MacBook-Pro-2.local>
2023-02-09 14:14:33 -0800

    docstrings

--- reportgen/analysis_modules/project_summary.py
+++ reportgen/analysis_modules/project_summary.py
@@ -1,21 +1,33 @@
 from .base import BaseAnalysis
 from .dataset_summary import DatasetSummary
 from .model_summary import ModelSummary
-from ..output_modules import SimpleTextBlock, FormattedTextBlock, SimpleImage, FormattedTextStyle, SimpleTextStyle, AddBreak
+from ..output_modules import BaseOutput, SimpleTextBlock, FormattedTextBlock, SimpleImage,\
+                             FormattedTextStyle, SimpleTextStyle, AddBreak
 from ..output_modules.text_styles import PlainText, BoldText, ItalicText
+from typing import Optional, List, Sequence, Union
 
 
 class ProjectSummary(BaseAnalysis):
+    """
+    An analysis module which creates a summary of the models and datasets that exist in a Fiddler project.
+    """
     def __init__(self, project_id):
+        """
+        :param project_id: The project ID in the Fiddler platform.
+        """
         self.project_id = project_id
 
-    def run(self, api):
+    def run(self, api) -> List[BaseOutput]:
+        """
+        :param api: An instance of Fiddler python client.
+        :return: List of output modules.
+        """
         output_modules = []
 
         models = api.list_models(self.project_id)
         datasets = api.list_datasets(self.project_id)
 
-        #output_modules += [SimpleImage('reportgen/output_modules/figures/fiddler_logo.png', width=2)]
+        output_modules += [SimpleImage('reportgen/output_modules/figures/fiddler_logo.png', width=2)]
         output_modules += [
                             SimpleTextBlock(text='Project Summary', style=SimpleTextStyle(alignment='center', font_style='bold', size=22)),
                             FormattedTextBlock(
@@ -28,7 +40,7 @@ class ProjectSummary(BaseAnalysis):
                                                 [
                                                  PlainText('Project '),
                                                  BoldText(self.project_id),
-                                                 PlainText(' contains {} models on {} datasets as summarized below.'.format(len(models), len(datasets))),
+                                                 PlainText(' contains {} models and {} datasets as summarized below.'.format(len(models), len(datasets))),
 
                                                 ]
                                                ),
