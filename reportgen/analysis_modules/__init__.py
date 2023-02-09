4c6e05e42379d718bff306ba10a0699319a9eea8
Bashir Rastegarpanah <bashir@Bashirs-MacBook-Pro-2.local>
2023-02-09 14:17:30 -0800

    .

--- reportgen/analysis_modules/__init__.py
+++ reportgen/analysis_modules/__init__.py
@@ -2,5 +2,6 @@ from .base import BaseAnalysis
 from .project_summary import ProjectSummary
 from .model_summary import ModelSummary
 from .dataset_summary import DatasetSummary
+from .model_performance import ModelPerformance
 
-__all__ = ('BaseAnalysis', 'ProjectSummary', 'ModelSummary', 'DatasetSummary')
+__all__ = ('BaseAnalysis', 'ProjectSummary', 'ModelSummary', 'DatasetSummary', 'ModelPerformance')
