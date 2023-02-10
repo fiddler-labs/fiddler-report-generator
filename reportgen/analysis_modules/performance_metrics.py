99f3d2996b10c4e6f9d856702ba19a140ed15ca2
Bashir Rastegarpanah <bashir@Bashirs-MacBook-Pro-2.local>
2023-02-10 09:17:56 -0800

    docstrings and checks

--- reportgen/analysis_modules/performance_metrics.py
+++ reportgen/analysis_modules/performance_metrics.py
@@ -6,16 +6,27 @@ import fiddler as fdl
 
 
 class BinaryClassifierMetrics(BaseAnalysis):
-
+    """
+       An analysis module that creates a table of performance metrics for a given list of binary classification models.
+    """
     def __init__(self, project_id, model_list: List[str]):
+        """
+        :param project_id: Project ID in the Fiddler platform.
+        :param model_list: List of binary classification model names. If None all models in the project are used.
+        """
         self.project_id = project_id
-        self.models = model_list
+        self.models = model_list if model_list else api.list_models(self.project_id)
 
     def run(self, api):
         table_rows = []
 
         for model_id in self.models:
             model_info = api.get_model_info(self.project_id, model_id)
+            if not model_info.model_task == fdl.ModelTask.BINARY_CLASSIFICATION:
+                raise TypeError(
+                    f'Binary classifier metrics can be computed for binary classification models only.'
+                )
+
             path = ['scoring', api.v1.org_id, self.project_id, model_id]
 
             for dataset in model_info.datasets:
