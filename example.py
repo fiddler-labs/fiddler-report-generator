a8aa66822ed81338bff065a289e4b0cb338b57bb
Bashir Rastegarpanah <bashir@Bashirs-MacBook-Pro-2.local>
2023-07-13 13:04:19 -0700

    update

--- example.py
+++ example.py
@@ -22,9 +22,18 @@ generate_report(fiddler_api=api,
                 author='Bashir R'
                 )
 
+# # ------------------------ example 2 ------------------------
+analysis1 = PerformanceAnalysisSpec(model_id='logreg_all',
+                                    metric='accuracy',
+                                    interval_length='3D',
+                                    segment_col='home_ownership'
+                                    )
+
 generate_report(fiddler_api=api,
                 analysis_modules=[ProjectSummary(project_id="lending",
-                                                 start_time_delta='30D'),
+                                                 start_time_delta='30D',
+                                                 performance_analysis=[analysis1],
+                                                 ),
                                   FeatureImpact(project_id="lending"),
                                   FailureCaseAnalysis(project_id="lending"),
                                   ],
@@ -32,54 +41,3 @@ generate_report(fiddler_api=api,
                 output_path='lending',
                 author='Bashir R'
                 )
-
-# # ------------------------ example 2 ------------------------
-# analysis1 = PerformanceAnalysisSpec(model_id='logreg_all',
-#                                     metric='accuracy',
-#                                     interval_length='2D',
-#                                     )
-#
-# analysis2 = PerformanceAnalysisSpec(model_id='logreg_all',
-#                                     metric='f1_score',
-#                                     interval_length='2D',
-#                                     )
-#
-# generate_report(fiddler_api=api,
-#                 analysis_modules=[ProjectSummary(project_id="lending",
-#                                                  start_time_delta='30D',
-#                                                  performance_analysis=[analysis1, analysis2]
-#                                                  )
-#                                   ],
-#                 output_type=OutputTypes.DOCX,
-#                 output_path='example2',
-#                 author='Bashir R'
-#                 )
-
-# # ------------------------ example 3 ------------------------
-# analysis1 = PerformanceAnalysisSpec(model_id='logreg_all',
-#                                     metric='accuracy',
-#                                     interval_length='2D',
-#                                     segment_col='home_ownership'
-#                                     )
-#
-# analysis2 = PerformanceAnalysisSpec(model_id='logreg_all',
-#                                     metric='f1_score',
-#                                     interval_length='2D',
-#                                     segment_col='home_ownership'
-#                                     )
-#
-# generate_report(fiddler_api=api,
-#                 analysis_modules=[ProjectSummary(project_id="lending",
-#                                                  start_time_delta='30D',
-#                                                  performance_analysis=[analysis1,
-#                                                                        analysis2,
-#                                                                        analysis2,
-#                                                                        analysis2,
-#                                                                        analysis2,
-#                                                                        analysis2,]
-#                                                  )
-#                                   ],
-#                 output_type=OutputTypes.DOCX,
-#                 output_path='example3',
-#                 author='Bashir R'
-#                 )
