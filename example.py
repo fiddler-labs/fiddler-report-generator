db8e4c2a267ca8d35d43c6d92989a79f386ed0df
Bashir Rastegarpanah <bashir@Bashirs-MacBook-Pro-2.local>
2023-09-14 15:34:47 -0700

    new interface

--- example.py
+++ example.py
@@ -1,42 +1,27 @@
 import os
-import fiddler as fdl
+#import fiddler as fdl
 import pandas as pd
 from datetime import datetime, timedelta
 from reportgen import FiddlerReportGenerator, OutputTypes
 from reportgen.analysis_modules import ProjectSummary, PerformanceAnalysisSpec, FailureCaseAnalysis, FeatureImpact
 print(f"Running client version {fdl.__version__}")
 
-api = fdl.FiddlerApi(url='http://demo.fiddler.ai', org_id='demo', auth_token=os.getenv('fiddler_api_key'))
 
-generate_report(fiddler_api=api,
-                analysis_modules=[ProjectSummary(project_id="lending",
-                                                 start_time_delta='30D',
-                                                 ),
-                                  FeatureImpact(project_id="lending"),
-                                  FailureCaseAnalysis(project_id="lending"),
-                                  ],
-                output_type=OutputTypes.DOCX,
-                output_path='lending',
-                author='Bashir R'
-                )
-
-
-FRoG = FiddlerReportGenerator(api, author='Bashir R')
-FRoG = FiddlerReportGenerator(
-                               url='http://demo.fiddler.ai',
-                               org_id='demo',
-                               auth_token=os.getenv('fiddler_api_key'),
-                               author='Bashir R')
+FRoG = FiddlerReportGenerator(url='http://demo.fiddler.ai',
+                              org_id='demo',
+                              auth_token=os.getenv('fiddler_api_key'),
+                              author='Bashir R'
+                              )
 
 FRoG.generate_report(project_id='imdb_rnn',
                      analysis_modules=[ProjectSummary(start_time_delta='30D'), FeatureImpact(), FailureCaseAnalysis()],
                      output_path='imdb'
                      )
-FRoG.generate_report(project_id='lending',
-                     analysis_modules=[ProjectSummary(start_time_delta='30D'), FeatureImpact(), FailureCaseAnalysis()],
-                     output_path='lending'
-                     )
-# # ------------------------ example 1 ------------------------
+
+
+# ------------------------------- old user interface ----------------------------
+# api = fdl.FiddlerApi(url='http://demo.fiddler.ai', org_id='demo', auth_token=os.getenv('fiddler_api_key'))
+# ------------------------ example 1 ------------------------
 # generate_report(fiddler_api=api,
 #                 analysis_modules=[ProjectSummary(project_id="imdb_rnn",
 #                                                  start_time_delta='30D'),
@@ -47,19 +32,19 @@ FRoG.generate_report(project_id='lending',
 #                 output_path='imdb',
 #                 author='Bashir R'
 #                 )
-#
+
 # # ------------------------ example 2 ------------------------
-generate_report(fiddler_api=api,
-                analysis_modules=[ProjectSummary(project_id="lending",
-                                                 start_time_delta='30D',
-                                                 ),
-                                  FeatureImpact(project_id="lending"),
-                                  FailureCaseAnalysis(project_id="lending"),
-                                  ],
-                output_type=OutputTypes.DOCX,
-                output_path='lending',
-                author='Bashir R'
-                )
+# generate_report(fiddler_api=api,
+#                 analysis_modules=[ProjectSummary(project_id="lending",
+#                                                  start_time_delta='30D',
+#                                                  ),
+#                                   FeatureImpact(project_id="lending"),
+#                                   FailureCaseAnalysis(project_id="lending"),
+#                                   ],
+#                 output_type=OutputTypes.DOCX,
+#                 output_path='lending',
+#                 author='Bashir R'
+#                 )
 
 # # ------------------------ example 3 ------------------------
 # analysis1 = PerformanceAnalysisSpec(model_id='logreg_all',
